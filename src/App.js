2a5f5b3e85b6473ca5b5805bfe6ed89b5c8a453f
legonnexon <gildalendario@gmail.com>
2023-01-03 16:45:30 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	deleted:    src/App.test.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'
+const socket = io.connect("http://localhost:3001")
 
 function App() {
+
+  const sendMessage = () => {
+    // socket.emit()
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="Mensagem" />
+      <button onClick={sendMessage}>Submit</button>
     </div>
   );
 }
